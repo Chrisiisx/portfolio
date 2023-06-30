ad2ea1143f7ffdb215df149fd7ea4fa7f8859136
ChrisExe25 <portoschristian25@gmail.com>
2023-06-30 13:01:53 +0200

    fix(frontend): fix not showing website (?)

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -23,94 +23,91 @@ function LandingPage() {
     setShowMoreSkills(!showMoreSkills);
   };
   return (
-    <>
-      <body className='bg-darkBase'>
-        <div className='flex h-screen bg-darkBase'>
-          <div className="m-auto">
-            <h3 className="text-8xl text-whiteBase text-center">
-              <p className="text-cyanBase">
-                {`<`}
-                <Link to="/whoami" className="text-whiteBase cursor-pointer">Chris</Link>
-                {`/>`}
-              </p>
-            </h3>
-            <p className="text-center text-4xl text-paragraphBase">Christian Portoghesi</p>
-            <div className="flex justify-center mt-10">
-              <Link to="portfolio" className="bg-cyanBase hover:opacity-60 text-darkBase font-bold py-4 px-8 rounded-full transition duration-300 ease-in-out">
-                View Portfolio
-              </Link>
-            </div>
+    <body className='bg-darkBase'>
+      <div className='flex h-screen bg-darkBase'>
+        <div className="m-auto">
+          <h3 className="text-8xl text-whiteBase text-center">
+            <p className="text-cyanBase">
+              {`<`}
+              <Link to="/whoami" className="text-whiteBase cursor-pointer">Chris</Link>
+              {`/>`}
+            </p>
+          </h3>
+          <p className="text-center text-4xl text-paragraphBase">Christian Portoghesi</p>
+          <div className="flex justify-center mt-10">
+            <Link to="portfolio" className="bg-cyanBase hover:opacity-60 text-darkBase font-bold py-4 px-8 rounded-full transition duration-300 ease-in-out">
+              View Portfolio
+            </Link>
           </div>
         </div>
+      </div>
 
-        <section className="py-2">
-          <div className="container mx-auto bg-darkBase">
-            <h2 className="text-4xl text-center text-whiteBase font-bold mb-8">About Me</h2>
-            <p className="text-center text-paragraphBase text-lg mb-8">
-              I am a computer science student aspiring to become a Full Stack Developer.
-              With this website, I showcase my personal projects and my skills.
-            </p>
-            <p className="text-center text-paragraphBase text-lg">
-              Let's work together to bring your ideas to life!
-            </p>
-            <div className="flex justify-center mt-8"> {/* Added mt-8 for spacing */}
-              <button className="bg-cyanBase hover:opacity-60 text-darkBase font-bold py-4 px-8 rounded-full transition duration-300 ease-in-out">
-                More About Me
-              </button>
-            </div>
+      <section className="py-2">
+        <div className="container mx-auto bg-darkBase">
+          <h2 className="text-4xl text-center text-whiteBase font-bold mb-8">About Me</h2>
+          <p className="text-center text-paragraphBase text-lg mb-8">
+            I am a computer science student aspiring to become a Full Stack Developer.
+            With this website, I showcase my personal projects and my skills.
+          </p>
+          <p className="text-center text-paragraphBase text-lg">
+            Let's work together to bring your ideas to life!
+          </p>
+          <div className="flex justify-center mt-8"> {/* Added mt-8 for spacing */}
+            <button className="bg-cyanBase hover:opacity-60 text-darkBase font-bold py-4 px-8 rounded-full transition duration-300 ease-in-out">
+              More About Me
+            </button>
           </div>
-        </section>
+        </div>
+      </section>
 
-        <section className="py-16">
-          <div className="container mx-auto px-4">
-            <h2 className="text-4xl text-center text-whiteBase font-bold mb-8">My Skills</h2>
-            <div className="flex justify-center">
-              <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
-                {skills.slice(0, showMoreSkills ? skills.length : 4).map((skill, index) => (
-                  <div className="bg-darkBase rounded-lg p-4 shadow-2xl" key={index}>
-                    <div className="flex items-center justify-center mb-4">
-                      {skill.icon}
-                    </div>
-                    <p className="text-center text-whiteBase font-semibold mb-2">{skill.title}</p>
-                    <div className="w-32 h-4 bg-gray-400 rounded-full">
-                      <div
-                        className="h-full bg-cyanBase rounded-full"
-                        style={{ width: `${skill.percentage}%` }}
-                      ></div>
-                    </div>
-                    <p className="text-center text-paragraphBase mt-2">{skill.percentage}%</p>
+      <section className="py-16">
+        <div className="container mx-auto px-4">
+          <h2 className="text-4xl text-center text-whiteBase font-bold mb-8">My Skills</h2>
+          <div className="flex justify-center">
+            <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
+              {skills.slice(0, showMoreSkills ? skills.length : 4).map((skill, index) => (
+                <div className="bg-darkBase rounded-lg p-4 shadow-2xl" key={index}>
+                  <div className="flex items-center justify-center mb-4">
+                    {skill.icon}
                   </div>
-                ))}
-              </div>
-            </div>
-            <div className="flex justify-center mt-4">
-              <button
-                onClick={toggleSkillsVisibility}
-                className="bg-cyanBase hover:opacity-60 text-darkBase font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out"
-              >
-                {showMoreSkills ? 'Show less' : 'Show more'}
-              </button>
-            </div>
-          </div>
-        </section>
-
-        <section className="py-16">
-          <div className="container mx-auto px-4">
-            <h2 className="text-4xl text-center text-whiteBase font-bold mb-8">Contact Me</h2>
-            <p className="text-center text-paragraphBase text-lg mb-8">
-              If you have any questions or would like to discuss a project, feel free to reach out to me.
-              I'm always open to new opportunities and collaborations.
-            </p>
-            <div className="flex justify-center">
-              <a href="mailto:portoschristian25@gmail.com" className="bg-cyanBase hover:opacity-60 text-darkBase font-bold py-4 px-8 rounded-full transition duration-300 ease-in-out">
-                Get in Touch
-              </a>
+                  <p className="text-center text-whiteBase font-semibold mb-2">{skill.title}</p>
+                  <div className="w-32 h-4 bg-gray-400 rounded-full">
+                    <div
+                      className="h-full bg-cyanBase rounded-full"
+                      style={{ width: `${skill.percentage}%` }}
+                    ></div>
+                  </div>
+                  <p className="text-center text-paragraphBase mt-2">{skill.percentage}%</p>
+                </div>
+              ))}
             </div>
           </div>
-        </section>
-      </body>
+          <div className="flex justify-center mt-4">
+            <button
+              onClick={toggleSkillsVisibility}
+              className="bg-cyanBase hover:opacity-60 text-darkBase font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out"
+            >
+              {showMoreSkills ? 'Show less' : 'Show more'}
+            </button>
+          </div>
+        </div>
+      </section>
 
-    </>
+      <section className="py-16">
+        <div className="container mx-auto px-4">
+          <h2 className="text-4xl text-center text-whiteBase font-bold mb-8">Contact Me</h2>
+          <p className="text-center text-paragraphBase text-lg mb-8">
+            If you have any questions or would like to discuss a project, feel free to reach out to me.
+            I'm always open to new opportunities and collaborations.
+          </p>
+          <div className="flex justify-center">
+            <a href="mailto:portoschristian25@gmail.com" className="bg-cyanBase hover:opacity-60 text-darkBase font-bold py-4 px-8 rounded-full transition duration-300 ease-in-out">
+              Get in Touch
+            </a>
+          </div>
+        </div>
+      </section>
+    </body>
   );
 }
 
