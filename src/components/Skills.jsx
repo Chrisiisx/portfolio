d64527b7be70fe49181ec0fd67ca3098f8a9c01d
Chrisiisx <portoschristian25@gmail.com>
2024-05-14 20:28:59 +0200

    Refactory skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,26 +1,29 @@
-import React, { useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHtml5, faCss3, faJs, faReact, faPython, faJava } from '@fortawesome/free-brands-svg-icons';
-import { faFileCode } from '@fortawesome/free-regular-svg-icons';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHtml5, faJs, faReact, faPython, faJava, faNodeJs, faDiscord, faNfcDirectional } from '@fortawesome/free-brands-svg-icons';
 
+function scrollToContactMe() {
+    let contactMeDiv = document.getElementById('contactme');
+    contactMeDiv.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
 
 const Skills = () => {
     const [showMoreSkills, setShowMoreSkills] = useState(false);
+
     const skills = [
-        { icon: <FontAwesomeIcon icon={faHtml5} size="3x" color='white' />, title: 'HTML5', percentage: 100 },
-        { icon: <FontAwesomeIcon icon={faCss3} size="3x" color='white' />, title: 'CSS3', percentage: 80 },
-        { icon: <FontAwesomeIcon icon={faJs} size="3x" color='white' />, title: 'JavaScript', percentage: 85 },
-        { icon: <FontAwesomeIcon icon={faReact} size="3x" color='white' />, title: 'React', percentage: 75 },
-        { icon: <FontAwesomeIcon icon={faPython} size="3x" color='white' />, title: 'Python', percentage: 95 },
-        { icon: <FontAwesomeIcon icon={faJava} size="3x" color='white' />, title: 'Java', percentage: 50 },
-        { icon: <FontAwesomeIcon icon={faFileCode} size="3x" color='white' />, title: 'C++', percentage: 90 },
-        { icon: <FontAwesomeIcon icon={faFileCode} size="3x" color='white' />, title: 'C', percentage: 75 },
+        { icon: <FontAwesomeIcon icon={faHtml5} size="3x" color='white' />, title: 'HTML5', percentage: 100, url: "https://www.html.it/guide/guida-html5/" },
+        { icon: <FontAwesomeIcon icon={faJs} size="3x" color='white' />, title: 'JavaScript', percentage: 85, url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
+        { icon: <FontAwesomeIcon icon={faReact} size="3x" color='white' />, title: 'React', percentage: 65, url: "https://reactjs.org/" },
+        { icon: <FontAwesomeIcon icon={faPython} size="3x" color='white' />, title: 'Python', percentage: 70, url: "https://www.python.org/" },
+        { icon: <FontAwesomeIcon icon={faJava} size="3x" color='white' />, title: 'Java', percentage: 70, url: "https://www.java.com/" },
+        { icon: <FontAwesomeIcon icon={faNodeJs} size="3x" color='white' />, title: 'NodeJs', percentage: 80, url: "https://nodejs.org/" },
+        { icon: <FontAwesomeIcon icon={faDiscord} size="3x" color='white' />, title: 'Discord.js', percentage: 80, url: "https://discord.js.org/" },
+        { icon: <FontAwesomeIcon icon={faNfcDirectional} size="3x" color='white' />, title: 'Databases', percentage: 70, url: "https://datasciencedojo.com/blog/understanding-databases/" },
     ];
+
     const toggleSkillsVisibility = () => {
         setShowMoreSkills(!showMoreSkills);
     };
-
-    
 
     return (
         <section className="py-16">
@@ -28,23 +31,32 @@ const Skills = () => {
                 <h2 className="text-4xl text-center text-whiteBase mb-8 font-extrabold">My Skills</h2>
                 <div className="flex justify-center">
                     <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
-                        {skills.slice(0, showMoreSkills ? skills.length : 4).map((skills, index) => (
+                        {skills.slice(0, showMoreSkills ? skills.length : 4).map((skill, index) => (
                             <div className="bg-darkBase rounded-lg p-4 shadow-2xl" key={index}>
                                 <div className="flex items-center justify-center mb-4">
-                                    {skills.icon}
+                                    {skill.icon}
                                 </div>
-                                <p className="text-center text-whiteBase font-semibold mb-2">{skills.title}</p>
+                                <p className="text-center text-whiteBase font-semibold mb-2 hover:underline">
+                                    <a href={skill.url} target='_blank'>{skill.title}</a>
+                                </p>
                                 <div className="w-32 h-4 bg-gray-400 rounded-full">
                                     <div
                                         className="h-full bg-cyanBase rounded-full"
-                                        style={{ width: `${skills.percentage}%` }}
+                                        style={{ width: `${skill.percentage}%` }}
                                     ></div>
                                 </div>
-                                <p className="text-center text-paragraphBase mt-2">{skills.percentage}%</p>
+                                <p className="text-center text-paragraphBase mt-2">{skill.percentage}%</p>
                             </div>
                         ))}
                     </div>
                 </div>
+                {showMoreSkills && (
+                    <div className="flex justify-center mt-4">
+                        <p className="text-center text-paragraphBase text-lg mb-8">
+                            If you want to know more about my skills, <a className="underline hover:cursor-pointer" onClick={scrollToContactMe}>contact me.</a>
+                        </p>
+                    </div>
+                )}
                 <div className="flex justify-center mt-4">
                     <button
                         onClick={toggleSkillsVisibility}
@@ -55,7 +67,7 @@ const Skills = () => {
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default Skills
+export default Skills;
